345f2c88df655e278327c61bb4bff764d0b91811
rhounsell <53660397+rhounsell@users.noreply.github.com>
2026-02-17 18:16:42 -0500

    refactor: simplify cost multiplier handling in CostDataExtractor

--- custom_components/green_button/const.py
+++ custom_components/green_button/const.py
@@ -4,43 +4,7 @@ from typing import Final
 DOMAIN: Final = "green_button"
 # INTEGRATION_SERVICE_DATA_KEY = "integration_service"
 
-
-# class _CustomEntitiesParams:
-#     NAME: Final = "name"
-#     USAGE_POINT_HREF: Final = "usage_point_href"
-#     ENERGY_UNIT: Final = "energy_unit"
-#     COST_CURRENCY: Final = "cost_currency"
-
-
-# @final
-# class ConfigParams(_CustomEntitiesParams):
-#     @classmethod
-#     def make_schema(cls, hass: HomeAssistant) -> vol.Schema:
-#         currency = hass.config.currency
-#         return vol.Schema(
-#             {
-#                 vol.Required(cls.NAME): str,
-#                 vol.Required(cls.USAGE_POINT_HREF): str,
-#                 vol.Required(
-#                     cls.ENERGY_UNIT, default=UnitOfEnergy.KILO_WATT_HOUR
-#                 ): vol.In([unit.value for unit in UnitOfEnergy]),
-#                 vol.Required(cls.COST_CURRENCY, default=currency): cv.currency,
-#             }
-#         )
-
-
-# @final
-# class OptionsParams(_CustomEntitiesParams):
-#     @classmethod
-#     def make_schema(cls) -> vol.Schema:
-#         return vol.Schema(
-#             {
-#                 vol.Required(
-#                     cls.USAGE_POINT_HREF,
-#                 ): str,
-#                 vol.Required(cls.ENERGY_UNIT): vol.In(
-#                     [unit.value for unit in UnitOfEnergy]
-#                 ),
-#                 vol.Required(cls.COST_CURRENCY): cv.currency,
-#             }
-#         )
+# Default power of ten multiplier for cost values
+# Cost values in Green Button XML are typically in hundredths of cents,
+# so -5 converts them to dollars (e.g., 12345 * 10^-5 = $0.12345)
+DEFAULT_COST_POWER_OF_TEN_MULTIPLIER: Final = -5
